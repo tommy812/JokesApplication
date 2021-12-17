0314d74dda5f6029ad84359e8aff02fc8ab5ba25
Thomas Tamagni <TT449@student.aru.ac.uk>
2021-12-17 01:34:41 +0000

    Project upload

---
+++ CrashCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CrashCourse.Models;
+
+namespace CrashCourse.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CrashCourse.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ CrashCourse/Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrashCourse.Models
+{
+    public class Joke
+    {
+        //prop to auto gerate 2x tab
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        //use ctro
+        public Joke()
+        {
+                
+        }
+    }
+}
